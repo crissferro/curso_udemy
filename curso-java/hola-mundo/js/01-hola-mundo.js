37381738f100a79be8f3b959bd3228aaf4db7a36
criss <crissferro@gmail.com>
2019-08-27 00:31:48 -0300

    curso java

---
+++ curso-java/hola-mundo/js/01-hola-mundo.js
@@ -0,0 +1,5 @@
+// alert("Hola Mundo con JS"); 
+// alert("Probando JS"); 
+// alert("Aprendiendo JS");    
+// console.log("Muestra esto en la consola");
+
---
+++ curso-java/hola-mundo/js/02-variables.js
@@ -0,0 +1,16 @@
+//VARIABLES
+//Una variable es un contenedor de informacion.
+
+var pais = "Espana";
+var continente = "Europa";
+var antiguedad = "2019";
+var pais_y_continente = pais+''+continente;
+
+
+
+pais = "Mexico";
+continente = "latinoamerica";
+
+console.log(pais, continente, antiguedad);
+
+alert(pais_y_continente);
